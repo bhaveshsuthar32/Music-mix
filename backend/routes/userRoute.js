a3002f80fe4aeaf387119209aa5c634725a29e1c
Anand Suthar <anandsuthar956@gmail.com>
2023-12-31 14:52:17 +0530

    routes updation

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,86 +7,82 @@ const validateObjectId = require("../middlewares/validateObjectId");
 const BlacklistModel = require("../model/blacklistModel");
 require("dotenv").config();
 
-
 //create user
 router.post("/register", async (req, res) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
   try {
-    const user = await UserModel.findOne({ email })
+    const user = await UserModel.findOne({ email });
     if (user) {
-      return res.status(402).send({ message: "User already exits with given email" })
+      return res
+        .status(402)
+        .send({ message: "User already exits with given email" });
     }
 
-
-    const salt = await bcrypt.genSalt(Number(process.env.SALT))
-    const hashedPassword = await bcrypt.hash(password, salt)
+    const salt = await bcrypt.genSalt(Number(process.env.SALT));
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     let newUser = await new UserModel({
       ...req.body,
-      password: hashedPassword
+      password: hashedPassword,
     }).save();
 
-    res.status(200).send({ data: newUser, message: "User created successfully" })
-  } catch (error) {
-
-  }
-})
-
+    res
+      .status(200)
+      .send({ data: newUser, message: "User created successfully" });
+  } catch (error) {}
+});
 
 router.post("/login", async (req, res) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
   try {
-
     const user = await UserModel.findOne({ email });
     if (!user) {
-      return res.status(400).send({ message: "Invalid email and passoword" })
+      return res.status(400).send({ message: "Invalid email and passoword" });
     }
 
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
-      return res.status(400).send({ message: "Invalid password" })
+      return res.status(400).send({ message: "Invalid password" });
     }
 
     const token = user.generateAuthToken();
-    res.status(200).send({ message: "Login sucsessfull!", token })
-
+    res.status(200).send({ message: "Login sucsessfull!", token });
   } catch (error) {
-    res.status(500).send({ message: "Internal server error" })
+    res.status(500).send({ message: "Internal server error" });
   }
-})
+});
 
 router.post("/logout", async (req, res) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
     if (token) {
       await BlacklistModel.updateMany({ $push: { blacklist: [token] } });
-      res.status(200).send({ msg: "Logout sucsessfull" })
+      res.status(200).send({ msg: "Logout sucsessfull" });
     }
   } catch (error) {
-    res.status(400).send({ error: error.message })
+    res.status(400).send({ error: error.message });
   }
-})
-
+});
 
 //get all users
 router.get("/", admin, async (req, res) => {
   try {
-    const users = await UserModel.find()
-    res.status(200).send({ data: users })
+    const users = await UserModel.find();
+    res.status(200).send({ data: users });
   } catch (error) {
-    res.status(400).send({ message: error })
+    res.status(400).send({ message: error });
   }
-})
-
-
+});
 
 // get user by id
 router.get("/:id", [validateObjectId, auth], async (req, res) => {
   try {
-    const user = await UserModel.findById(req.params.id).select("-password -__v");
+    const user = await UserModel.findById(req.params.id).select(
+      "-password -__v"
+    );
     res.status(200).send({ data: user });
   } catch (error) {
-    res.status(400).send(error)
+    res.status(400).send(error);
   }
 });
 
@@ -106,66 +102,4 @@ router.delete("/:id", [validateObjectId, admin], async (req, res) => {
   res.status(200).send({ message: "Successfully deleted user." });
 });
 
-
-module.exports = router
-
-
-// const registerMiddle = require("../middlewares/auth.middleware");
-// const userModel = require("../model/userModel");
-// const bcrypt = require("bcrypt");
-// var jwt = require('jsonwebtoken');
-
-// const userRoute = require("express").Router();
-
-// userRoute.post("/register", async (req, res) => {
-//   const { name, email, password, gender } = req.body;
-//   try {
-//     let findUser = await userModel.findOne({ email });
-
-//     if (findUser) {
-//       res.json({ msg: "User has already registered please login" });
-//     }
-
-//     bcrypt.hash(password, 5, async (err, hash) => {
-//       if (hash) {
-//         const data = await userModel.create({
-//           name,
-//           email,
-//           password: hash,
-//           gender
-//         });
-//         res.json({ msg: "User register successfully", user: data });
-//       }
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-
-
-// userRoute.post("/login", async (req, res) => {
-//   const { email, password } = req.body;
-//   try {
-//     let findUser = await userModel.findOne({ email });
-
-//     if (findUser) {
-//       bcrypt.compare(password, findUser.password, function (err, result) {
-//         if (result) {
-//           var token = jwt.sign({ userId: findUser._id }, 'music');
-//           res.json({ msg: "User login succesfully", token })
-//         }
-//         else {
-//           res.json({ msg: "Wrong password please enter valid password to login" })
-//         }
-//       });
-//     }
-//     else {
-//       res.json({ msg: "Wrong email and password !" })
-//     }
-
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-
-// module.exports = userRoute;
+module.exports = router;
